aff9632e749f52fe848a41c49db6b94c05fb34c2
Beamlak Gebre <beamlaktg@gmail.com>
2021-03-30 23:42:17 -0400

    I have added exercise.py

---
+++ exercise.py
@@ -0,0 +1,10 @@
+"""" Simple movie database with actors/actresses. """
+
+class Cast:
+    """" Actors/actresses in a film. 
+    
+    Attributes:
+        name (str): the actors/actresses name.
+        film (set of Cast): films in which actors/actresses star in. 
+    """
+    
