a53fe87e256fb35f71b353a0328dea50e95992ad
gmahapat <63254417+gmahapat@users.noreply.github.com>
2021-04-13 19:41:16 -0400

    Update exercise.py
    Added a place holder

--- exercise.py
+++ exercise.py
@@ -49,5 +49,31 @@ class Cast:
     read in the csv file of movies with their attached actors
     
     """
+    #This needs attention
+    
+    def parse_args(arglist):
+    """ Process command line arguments.
+    
+    Expect one mandatory argument (a frequency) and one optional argument,
+    preceded by "-a4" (the value to use as the frequency of A4).
+    
+    Args:
+        arglist (list of str): arguments from the command line.
+    
+    Returns:
+        namespace: the parsed arguments, as a namespace.
+    """
+    parser = ArgumentParser()
+    parser.add_argument("freq", type=float, help="a frequency in Hz")
+    parser.add_argument("-a4", type=float, default=440.0,
+                        help="frequency to use for A4")
+    args = parser.parse_args(arglist)
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv[1:])
+    main(args.freq, a4=args.a4)
+
     
     
