b3ff025ad3f97cc9ec8ff3e0b7cd3286109219c8
gmahapat <63254417+gmahapat@users.noreply.github.com>
2021-04-16 10:51:56 -0400

    Made a few updates during class

--- exercise.py
+++ exercise.py
@@ -6,7 +6,7 @@ class Cast:
     Actors/actresses to a film. 
     
     Attributes:
-        name (str): the actors/actresses name.
+        name (list): the actors/actresses name.
         film (str): name of a movie. 
     """
     def __init__(self, name, film):
@@ -14,7 +14,7 @@ class Cast:
         initialize the attributes of a class. 
         
         Args:
-            name(str): name of actors/actress.
+            name(list): name of actors/actress.
             film(str): name of film.
         
         """
@@ -40,9 +40,6 @@ class Cast:
         Returns: 
             cast_film(tuple): a tuple of the actor/actress along with the film they are associted with. 
         """
-        
- class Output:
-    """Will retrieve appropriate data and order the data in alphabetical order."""
     
     def order_cast(film):
         """Get the correct names for the movie and list in alphabetical order.
@@ -67,6 +64,11 @@ class Cast:
     """This class will be used to display the information in the file with the actors 
     and their movies/ or their movie appearances."""
     
+    def __init__(self, name):
+        with open('movies.csv', 'w', newline='') as csvfile:
+        movieInfo = ['Movie', 'Actors']
+        writer = csv.DictWriter(csvfile, movieInfo = movieInfo)
+    
     def display_data():
         data = pd.read_csv("filename.csv", sep = ',')
         data
@@ -76,46 +78,14 @@ class Cast:
         total_appearance = appearances.isin(["Actor name goes here"])
         total_appearance
         
- class Movies:
-    """ Movies from the csv file
-    
-    Attributes:
-        title(str): the title of the movies
-        actors(str): all of the actors from each respective movie    
-    """
-    def __init__(self, name):
-        with open('movies.csv', 'w', newline='') as csvfile:
-        movieInfo = ['Movie', 'Actors']
-        writer = csv.DictWriter(csvfile, movieInfo = movieInfo)
-    """
-    read in the csv file of movies with their attached actors
-    
-    """
     #This needs attention
     
     def parse_args(arglist):
-    """ Process command line arguments.
-    
-    Expect one mandatory argument (a frequency) and one optional argument,
-    preceded by "-a4" (the value to use as the frequency of A4).
-    
-    Args:
-        arglist (list of str): arguments from the command line.
-    
-    Returns:
-        namespace: the parsed arguments, as a namespace.
-    """
-    parser = ArgumentParser()
-    parser.add_argument("freq", type=float, help="a frequency in Hz")
-    parser.add_argument("-a4", type=float, default=440.0,
-                        help="frequency to use for A4")
-    args = parser.parse_args(arglist)
-    return args
+   #user input
 
 
 if __name__ == "__main__":
-    args = parse_args(sys.argv[1:])
-    main(args.freq, a4=args.a4)
+   
 
     
     
